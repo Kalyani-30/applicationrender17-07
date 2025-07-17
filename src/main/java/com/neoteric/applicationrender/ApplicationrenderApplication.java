f3109986cd393a61bcfbdf7da233dc227fe86315
KALYANI <gandukalyani1@gmail.com>
2025-07-17 14:31:47 +0530

    Deploy Spring Boot app to Render

---
+++ src/main/java/com/neoteric/applicationrender/ApplicationrenderApplication.java
@@ -0,0 +1,13 @@
+package com.neoteric.applicationrender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationrenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationrenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoteric/applicationrender/ApplicationrenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric.applicationrender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationrenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
